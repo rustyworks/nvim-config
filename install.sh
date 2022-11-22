287c26ddebfedaa4a207aba6bb6210b02ee273f5
Tristanto <tristanto.kurniawan@gmail.com>
2022-11-22 16:25:27 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd ~
 ln -sf $CURRENT_PATH/.nvimrc .nvimrc
 ln -sf $CURRENT_PATH/.nvim .nvim
 
+mkdir ~/.config
 mkdir ~/.config/nvim
+touch ~/.config/nvim/init.vim
 
 echo 'set runtimepath^=~/.nvim' >> ~/.config/nvim/init.vim
 echo 'let &packpath=&runtimepath' >> ~/.config/nvim/init.vim
 echo 'source ~/.nvimrc' >> ~/.config/nvim/init.vim
-
-# git clone 'https://github.com/VundleVim/Vundle.vim' $CURRENT_PATH/.vim/bundle/Vundle.vim
